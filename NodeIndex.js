c8db499c8e7adc3c018f976d0e6bbf3e919d0eb1
kzbulti <50126169+kzbulti@users.noreply.github.com>
2023-06-29 19:15:30 +0800

    Made nodejs work properly

--- NodeIndex.js
+++ NodeIndex.js
@@ -1,30 +1,76 @@
 var http = require('http');
 var fs = require('fs');
+const path = require('path');
 
-http.createServer(function (req, res) {
-  if (req.url === "/cats") {
+http.createServer(onRequest).listen(8080);
+
+function onRequest(request, response) {
+  if (request.url.startsWith("/public/")) {
+	fs.readFile(path.join(__dirname, path.normalize(request.url)), function (err,data) {
+	  if (err) {
+		response.writeHead(404);
+		response.end(JSON.stringify(err));            
+		return;
+	  }
+	response.writeHead(200);
+	response.end(data);
+  });
+
+  return;
+}
+
+  if (request.url == "/cat.html") {	  
     fs.readFile('cat.html', function(err, data) {
       if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found');
-        return res.end();
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('404 Not Found');
+        return response.end();
+      }
+
+      
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      return response.end();
+    });
+  } else if (request.url == "/vote.html") {
+	
+    fs.readFile('vote.html', function(err, data) {
+      if (err) {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('404 Not Found');
+        return response.end();
       }
       
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      return response.end();
     });
-  } else {
+  } else if (request.url == "/index.html" ) {
+	
     fs.readFile('index.html', function(err, data) {
       if (err) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found');
-        return res.end();
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('404 Not Found');
+        return response.end();
       }
       
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      return response.end();
     });
   }
-}).listen(8080);
+  else {
+	
+	fs.readFile('index.html', function(err, data) {
+		if (err) {
+		  response.writeHead(404, {'Content-Type': 'text/plain'});
+		  response.write('404 Not Found');
+		  return response.end();
+		}
+		
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(data);
+		return response.end();
+	  });
+  }
+}
