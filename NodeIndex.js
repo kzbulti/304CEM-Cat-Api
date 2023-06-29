e266bc9e1ec8cb1417a4ebc43d15ca65b99f1a27
kzbulti <50126169+kzbulti@users.noreply.github.com>
2023-06-29 17:49:46 +0800

    Update NodeIndex.js

--- NodeIndex.js
+++ NodeIndex.js
@@ -1,17 +1,30 @@
 var http = require('http');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
-	if (req.url= "/cats") {
-		fs.readFile('cat.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		return res.end();
+  if (req.url === "/cats") {
+    fs.readFile('cat.html', function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found');
+        return res.end();
+      }
+      
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    fs.readFile('index.html', function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found');
+        return res.end();
+      }
+      
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
   }
-  else {
-		fs.readFile('index.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		return res.end();
-  }
-  });
 }).listen(8080);
